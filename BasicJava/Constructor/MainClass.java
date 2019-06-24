e9631239b30087d5cb961a55c78f2672c89aaed9
sokhomsovichea <45678328+sokhomsovichea@users.noreply.github.com>
2019-06-24 10:11:50 +0700

    Create MainClass.java

---
+++ BasicJava/Constructor/MainClass.java
@@ -0,0 +1,8 @@
+public class constructor {
+    public static void main(String args[]){
+        conclass conObj = new conclass("Kyrie 5");
+        conclass conObj2 = new conclass("Kyrie 4");
+        conObj.say();
+        conObj2.say();
+    }
+}
