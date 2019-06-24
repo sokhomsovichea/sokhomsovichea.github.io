d6440f153661e7f5fe2164ed12e4b86aabaa4212
sokhomsovichea <45678328+sokhomsovichea@users.noreply.github.com>
2019-06-24 10:08:17 +0700

    Create Main.java

---
+++ BasicJava/Fried Chicken/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class multiple_classes {
+
+    public static void main(String[] args){
+        Chicken Menu = new Chicken();// Menu is the object name and we have to assign it as equal to the new and empty parameter obj
+        Menu.ChickenDish();//first call the object name then use the . separator and the class's obj name
+
+
+        /*Scanner inf = new Scanner(System.in);
+        System.out.println("Enter you name: ");
+        String name = inf.nextLine();
+        Chicken iden = new Chicken();
+        iden.Info(name);*/
+
+    }
+}
